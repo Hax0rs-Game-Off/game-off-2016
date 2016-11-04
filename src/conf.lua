3a9ff9cb3e47ee247265996acb762cc6430877a8
Nicholas Irving <Wast3r88@gmail.com>
2016-11-04 18:58:12 +1000

    screen size + resizable

--- src/conf.lua
+++ src/conf.lua
@@ -5,12 +5,12 @@ function love.conf(t)
     t.version = "0.10.2" -- Löve version
     t.console = false
     t.release = false
-    t.window.width = 400
-    t.window.height = 300
+    t.window.width = 1280
+    t.window.height = 720
     t.window.fullwindow = false
     t.window.vsync = true
     t.window.fsaa = 0
-
+	t.window.resizable = true
     t.modules.joystick = false
     t.modules.audio = false
     t.modules.keyboard = true
